68e5cfbd74ce040545403b0242a3412caa56e43d
WAMclaw <mcclainwa@g.cofc.edu>
2021-08-31 10:13:11 -0400

    William McClain, CSCI 220-02, hw1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,11 @@
+#William McClain
+#CSCI 220-02
+#8/31/21
+#Code prints hello, world!
+
+def main():
+    print("hello, world!")
+#Defines main as a function that prints hello, world!
+
+main()
+#The Code executes the main function
