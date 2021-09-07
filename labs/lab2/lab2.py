7a9ffb841a104adaa06408aeaa60bbeaf37a844c
WAMclaw <mcclainwa@g.cofc.edu>
2021-09-07 16:15:36 -0400

    incomplete lab2

--- labs/lab2/lab2.py
+++ labs/lab2/lab2.py
@@ -1,7 +1,8 @@
 """
 Name: William McClain
 lab2.py
-Problem: create code that
+Problem: create function that creates the sum of threes chosen by a user, create a function that makes a multiplication table
+create a function that adds squareroots of user inputs, create a function that manually calculates a square
 """
 import math
 
@@ -10,7 +11,7 @@ def sum_of_threes():
     bound_sot = eval(input("Please give an upper bound: "))
     variable_of_three = 0
     for in_three in range(3,bound_sot+1,3):
-        var_of_three = variable_of_three + in_three
+        variable_of_three = variable_of_three + in_three
     print("The sum of the threes is ", variable_of_three)
 
 
@@ -37,14 +38,13 @@ def sumSquares():
     lowerb = eval(input("Enter a lower bound:"))
     upperb = eval(input("Enter an upper bound: "))
     pwr = 0
-    pwrsum = 0
     for add in range(lowerb,upperb+1):
         pwr = add ** 2
         #print(pwr)
-        pwrsum = pwr + pwr
+
         #print(pwrsum)
 
-    print("The sum of your squares is ",pwrsum)
+    print("The sum of your squares is ",pwr)
 
 
 
